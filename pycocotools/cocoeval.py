c9005b43637f6763aff9bd7a7d1282a87d2242ee
Polaris <polaris0532@outlook.com>
2025-04-27 21:59:42 +0800

    修复_summarizeDets显示，对齐原本pycocotools

--- pycocotools/cocoeval.py
+++ pycocotools/cocoeval.py
@@ -588,19 +588,22 @@ class COCOeval:
             return mean_s
 
         def _summarizeDets():
-            stats = np.zeros((12,))
-            stats[0] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[1] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.5, areaRng='tiny', maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            stats = np.zeros((14,))
+            stats[0]  = _summarize(1,                                 maxDets=self.params.maxDets[2])
+            stats[1]  = _summarize(1, iouThr=.5,                      maxDets=self.params.maxDets[2])
+            stats[2]  = _summarize(1, iouThr=.75,                     maxDets=self.params.maxDets[2])
+            stats[3]  = _summarize(1,                areaRng='small', maxDets=self.params.maxDets[2])
+            stats[4]  = _summarize(1,                areaRng='medium',maxDets=self.params.maxDets[2])
+            stats[5]  = _summarize(1,                areaRng='large', maxDets=self.params.maxDets[2])
+
+            stats[6]  = _summarize(0,                                 maxDets=self.params.maxDets[0])
+            stats[7]  = _summarize(0,                                 maxDets=self.params.maxDets[1])
+            stats[8]  = _summarize(0,                                 maxDets=self.params.maxDets[2])
+            stats[9]  = _summarize(0,                areaRng='small', maxDets=self.params.maxDets[2])
+            stats[10] = _summarize(0,                areaRng='medium',maxDets=self.params.maxDets[2])
+            stats[11] = _summarize(0,                areaRng='large', maxDets=self.params.maxDets[2])
+            stats[12] = _summarize(0, iouThr=.5,                      maxDets=self.params.maxDets[2])
+            stats[13] = _summarize(0, iouThr=.75,                     maxDets=self.params.maxDets[2])
             return stats
         def _summarizeDets_TOD():
             stats = np.zeros((12,))
@@ -675,8 +678,8 @@ class Params:
             }
         else:
             self.maxDets = [1, 10, 100]
-            self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 16 ** 2], [16 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-            self.areaRngLbl = ['all',               'tiny',             'small',             'medium',          'large']
+            self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+            self.areaRngLbl = ['all',               'small',             'medium',          'large']
         self.useCats = 1
 
     def setKpParams(self):
